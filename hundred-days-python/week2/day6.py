9ebc1b86ffc470ec335781d54d4cd1ff813a0129
domwat <dominicwaters12@gmail.com>
2021-02-01 09:16:34 -0600

    finish day 6

---
+++ hundred-days-python/week2/day6.py
@@ -0,0 +1,19 @@
+# Functions, Code Blocks, and While loops 
+
+# # FUNCTIONS
+# # start with 'def' key word then 'name' and '()' end with ':'
+# def prac_function():
+#     print('Hello')
+#     print('Bye')
+
+# # to trigger the function you have to call it
+# prac_function()
+
+
+# WHILE LOOP
+# 'while' 'condition': 'effect'
+# be careful to give an ending otherwise you will get an infinite loop
+num = 0
+while num <= 5:
+    print(num)
+    num += 1
